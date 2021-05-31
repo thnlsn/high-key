c00bd072e134377d7e1a0d5185a141bd5475055a
Thomas <thnlsn.dev@gmail.com>
2021-05-31 11:37:40 -0700

    added a selector for selection state

--- extension.js
+++ extension.js
@@ -1,6 +1,6 @@
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
-const vscode = require('vscode');
+const vscode = require("vscode");
 
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
@@ -9,28 +9,40 @@ const vscode = require('vscode');
  * @param {vscode.ExtensionContext} context
  */
 function activate(context) {
+  console.log('Congratulations, your extension "high-key" is now active!');
 
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "high-key" is now active!');
+  // The command has been defined in the package.json file
+  // Now provide the implementation of the command with registerCommand
+  // The commandId parameter must match the command field in package.json
+  let disposable = vscode.commands.registerCommand(
+    "high-key.helloWorld",
+    function () {
+      // The code you place here will be executed every time your command is executed
 
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with  registerCommand
-	// The commandId parameter must match the command field in package.json
-	let disposable = vscode.commands.registerCommand('high-key.helloWorld', function () {
-		// The code you place here will be executed every time your command is executed
+      setTimeout(() => {
+        const editor = vscode.window.activeTextEditor;
+        const selection = editor.selection;
+        console.log(selection);
 
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World from high-key!');
-	});
+        const position = editor.selection.active;
+        console.log(position);
 
-	context.subscriptions.push(disposable);
+        const text = editor.document.getText(selection);
+        // vscode.window.showInformationMessage(text || "yo what up");
+      }, 2000);
+
+      // Display a message box to the user
+      vscode.window.showInformationMessage("Hello World from high-key!");
+    },
+  );
+
+  context.subscriptions.push(disposable);
 }
 
 // this method is called when your extension is deactivated
 function deactivate() {}
 
 module.exports = {
-	activate,
-	deactivate
-}
+  activate,
+  deactivate,
+};
